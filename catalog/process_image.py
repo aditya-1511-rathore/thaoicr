bb84f570f494a22ba5d9a430698413c003f3a9fa
aditya-1511-rathore <72315357+aditya-1511-rathore@users.noreply.github.com>
2023-12-26 14:42:36 +0530

    Update process_image.py

--- catalog/process_image.py
+++ catalog/process_image.py
@@ -21,8 +21,11 @@ def get_id(inputs):
     id = None
     for i in inputs:
         x = re.search(r"[0-9]\s[0-9][0-9][0-9][0-9]\s[0-9][0-9][0-9][0-9][0-9]\s[0-9][0-9]\s[0-9]", i)
+        print(x)
         if x is not None:
             id = x.group()
+        for m in re.finditer(r"[0-9]\s[0-9][0-9][0-9][0-9]\s[0-9][0-9][0-9][0-9][0-9]\s[0-9][0-9]\s[0-9]",i):
+            print(m.start(), m.end())
 
 
 def get_name(inputs):
